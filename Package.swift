cd48b8fdd60e79fa72f535b3d8f6ff11a0c47fa1
Kyle Jessup <badthing@gmail.com>
2017-10-16 16:34:58 -0400

    Bumped dep versions to 3

--- Package.swift
+++ Package.swift
@@ -23,9 +23,9 @@ let package = Package(
 	name: "PerfectFileMaker",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2)
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3)
 	],
 	exclude: []
 )
